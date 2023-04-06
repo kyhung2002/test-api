30453d15d3c5bc6029000a17fa06c6b1b99f1621
kyhung2002 <trankyhung225@gmail.com>
2023-04-06 18:57:51 +0700

    test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const verifyToken = require("./middleware/auth");
 let rawdata = fs.readFileSync("db.json");
 let database = JSON.parse(rawdata);
+const jsonServer = require("json-server");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
